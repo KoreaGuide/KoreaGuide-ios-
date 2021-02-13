f571b95759e4c1143463cf36be3b5ca2e578ce7c
Chi hyun <alzee0381@gmail.com>
2021-02-13 17:58:45 +0900

    Initial Commit

---
+++ KoreaGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KoreaGuide
+//
+//  Created by Song chi hyun on 2021/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
