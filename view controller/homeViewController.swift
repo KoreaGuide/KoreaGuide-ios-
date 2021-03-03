d1365564f55139af7b14fe9b651f4a36ea85f557
Chi hyun <alzee0381@gmail.com>
2021-03-03 20:41:20 +0900

    home view 만듬

---
+++ view controller/homeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  homeViewController.swift
+//  KoreaGuide
+//
+//  Created by Song chi hyun on 2021/03/03.
+//
+
+import Foundation
+import UIKit
+
+class homeViewController: UIViewController {
+    @IBOutlet var menuButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
