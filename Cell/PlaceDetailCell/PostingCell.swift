980e110aa1c0bbb7cf4f3719bfbb9cec4f79b39f
Chi hyun <alzee0381@gmail.com>
2021-04-06 01:23:51 +0900

    seg button 누르면 텍스트 바뀜

--- Cell/PlaceDetailCell/PostingCell.swift
+++ Cell/PlaceDetailCell/PostingCell.swift
@@ -13,10 +13,22 @@ final class PostingCell: UICollectionViewCell {
   @IBOutlet var title_eng: UILabel!
   @IBOutlet var overview: UITextView!
   @IBOutlet var seg : UISegmentedControl!
+  var overview_kor : String?
+  var overview_eng : String?
   override func bindViewModel(_ viewModel: Any) {
     guard let viewModel = viewModel as? PostingViewModel else { return }
     title_kor.text = viewModel.title
     title_eng.text = viewModel.title
     overview.text = viewModel.overview_Kor
+    overview_eng = viewModel.overview_Eng
+    overview_kor = viewModel.overview_Kor
+  }
+  
+  @IBAction func sgChangeLanguage(_ sender: UISegmentedControl) {
+    if sender.selectedSegmentIndex == 0 {
+      self.overview.text = overview_kor
+    } else if sender.selectedSegmentIndex == 1 {
+      self.overview.text = overview_eng
+    }
   }
 }
