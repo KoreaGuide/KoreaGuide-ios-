84a8f5c37cdddc72ccf813496903eaa3982d2164
imseonho <seonandshine@icloud.com>
2021-04-05 16:41:32 +0900

    wip

--- ViewModel/WordViewModel.swift
+++ ViewModel/WordViewModel.swift
@@ -1,1 +1,45 @@
+//
+//  WordViewModel.swift
+//  KoreaGuide
+//
+//  Created by 임선호 on 2021/04/05.
+//
 
+import Foundation
+import IGListKit
+
+final class WordList : ListDiffable {
+    
+    
+    
+    func diffIdentifier() -> NSObjectProtocol {
+        <#code#>
+    }
+    
+    func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
+        <#code#>
+    }
+    
+    
+}
+
+class Vocab : ListDiffable {
+    func diffIdentifier() -> NSObjectProtocol {
+        <#code#>
+    }
+    
+    func isEqual(toDiffableObject object: ListDiffable?) -> Bool {
+        <#code#>
+    }
+    
+    let vocabTitle: String
+    let vocabTotalCount: Int
+    let vocabCompleteCount: Int
+    
+    init(){
+        self.vocabTitle = ""
+        self.vocabTotalCount = 0
+        self.vocabCompleteCount = 0
+    }
+    
+}
