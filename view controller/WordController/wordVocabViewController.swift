339c2edce7c596a239b2c81c5b03951d2a018310
imseonho <seonandshine@icloud.com>
2021-04-06 02:42:39 +0900

    wip 210406

--- view controller/WordController/wordVocabViewController.swift
+++ view controller/WordController/wordVocabViewController.swift
@@ -11,12 +11,22 @@ import UIKit
 class wordVocabViewController : UIViewController {
     
     @IBOutlet weak var vocabTopLabel: UILabel!
+  
     @IBOutlet weak var addedFolder: UIView!
     @IBOutlet weak var learningFolder: UIView!
     @IBOutlet weak var completeFolder: UIView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addedFolder.layer.cornerRadius = 10;
+        learningFolder.layer.cornerRadius = 10;
+        completeFolder.layer.cornerRadius = 10;
+        
+        addedFolder.alpha = 0.8;
+        
+        
     }
 }
 
