20f0e85e67d38f9e8c2094ca56e4c247bfebbb24
imseonho <seonandshine@icloud.com>
2021-04-07 17:30:30 +0900

    wip

--- view controller/WordController/wordListViewController.swift
+++ view controller/WordController/wordListViewController.swift
@@ -9,7 +9,19 @@ import Foundation
 import UIKit
 
 class wordListViewController : UIViewController {
+    let wordStoryboard : UIStoryboard = UIStoryboard(name: "Word", bundle: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func learnButton(_ sender: Any) {
+        let wordLearnViewController = wordStoryboard.instantiateViewController(identifier: "wordLearnView")
+        self.show(wordLearnViewController, sender: self)
+        
+    }
+    
+    @IBAction func testButton(_ sender: Any) {
+        let testTypeChoiceViewController = wordStoryboard.instantiateViewController(identifier: "testListView")
+        self.show(testTypeChoiceViewController, sender: self)
     }
 }
